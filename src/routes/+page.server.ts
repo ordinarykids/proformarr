15706b3e809e7468d936f552749a9fc463a96c45
Jason Herring <jason@jasons-mbp.lan>
2024-07-01 20:50:00 -0700

    day 1

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,60 @@
+import { message, superValidate } from 'sveltekit-superforms';
+import { zod } from 'sveltekit-superforms/adapters';
+import { fail } from '@sveltejs/kit';
+import { schema } from './schema.ts';
+import Anthropic from "@anthropic-ai/sdk";
+import { ANTHROPIC_API_KEY } from '$env/static/private';
+
+export const load = async () => {
+  const form = await superValidate(zod(schema));
+
+  // Always return { form } in load functions
+  return { form };
+};
+
+export const actions = {
+  default: async ({ request }) => {
+    const form = await superValidate(request, zod(schema));
+    console.log(form);
+
+    if (!form.valid) {
+      // Again, return { form } and things will just work.
+      return fail(400, { form });
+    }
+
+    // TODO: Do something with the validated form.data
+
+
+    const anthropic = new Anthropic({
+      apiKey: ANTHROPIC_API_KEY, // defaults to process.env["ANTHROPIC_API_KEY"]
+    });
+
+    const msg = await anthropic.messages.create({
+      model: "claude-3-5-sonnet-20240620",
+      max_tokens: 1000,
+      temperature: 0,
+      messages: [
+     
+        {
+          "role": "user",
+          "content": [
+            {
+              "type": "text",
+              "text": "give me a detailed prorforma for 1250 Lerida Way, Pacifica CA 94044\n. Only return the HTML part of a svelte component. No script, no explanation"
+            }
+          ]
+        }
+      ]
+    });
+    console.log(msg);
+
+
+
+    return message(form, msg);
+  }
+};
+
+
+
+
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,65 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { Input } from "$lib/components/ui/input/index.js";
+    import { Button } from "$lib/components/ui/button";
+    import { superForm } from 'sveltekit-superforms';
+    import type { PageData } from './$types';
+
+    export let data: PageData;
+
+    const { form, errors, constraints, message, enhance } = superForm(data.form);
+
+
+</script>
+
+
+
+
+<div class="w-1/2 m-auto  md:w-2/3 lg:w-1/2 mt-16">
+    <div>
+        <img src="proformarr-owl.png" class="w-full"/>
+
+    </div>
+    <div class="w-full px-4 py-8 m-auto">
+        <h1 class='font-bold text-xl'>PROFORMARR</h1>
+       
+        <form method="POST" use:enhance>
+    
+            <Input class="my-4"
+                type="text"
+                name="name"
+                aria-invalid={$errors.name ? 'true' : undefined}
+                bind:value={$form.name}
+                {...$constraints.name}
+            />
+            {#if $errors.name}<span class="invalid">{$errors.name}</span>{/if}
+
+         
+            <!-- {#if $errors.email}<span class="invalid">{$errors.email}</span>{/if} -->
+
+            <div class='p-2 w-32 text-center bg-amber-200'><button>Submit</button></div>
+
+        </form>
+        
+
+            {#if $message}
+            <div class='p-4 bg-yellow-100'>
+                {@html $message.content[0].text}
+            </div>
+            {/if}
+    
+         
+        
+      
+ 
+    </div>
+    
+
+
+
+</div>
+
+<style>
+    .invalid {
+        color: red;
+    }
+</style>
